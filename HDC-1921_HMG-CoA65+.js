080c961e1836583b199b3bc857fc2e67b2202c84
Derek Roberts <derek.roberts@gmail.com>
2016-03-08 22:14:21 -0800

    1921 output - pid, title, date and category in JSON

--- HDC-1921_HMG-CoA65+.js
+++ HDC-1921_HMG-CoA65+.js
@@ -4,39 +4,40 @@
 function map( patient ){
 
   // Physician ID, value type and time interval
-  var pid   = patient.json.primary_care_provider_id;
-  var title = 'HDC-1921';
-  var type  = 'ratio';
-  var now   = new Date();
-  var date  = new Date( now.getFullYear(), now.getMonth(), 1).getTime();
+  var pid      = patient.json.primary_care_provider_id;
+  var title    = 'HDC-1921';
+  var category = 'ReportingCategories';
+  var type     = 'ratio';
+  var now      = new Date();
+  var date     = new Date( now.getFullYear(), now.getMonth(), 1).getTime();
 
   // Active patient? Age 65+?  Active statin?
-  var ap = activePatient( patient );
-  var ia = isAge( patient, 65 );
-  var hm = hasActiveStatin( patient );
+  var active   = activePatient( patient );
+  var is_age   = isAge( patient, 65 );
+  var statin   = hasActiveStatin( patient );
 
   // Assemble query
-  var denominator = ap && ia;
-  var numerator   = denominator && hm;
+  var denominator = active && is_age;
+  var numerator   = denominator && statin;
 
   // Emits
   emit(
     '{ '+
-      '"doctor" : "' + pid  + '", ' +
-      '"date" : "'   + date + '", ' +
-      '"type" : "'   + type + '", ' +
-      '"result" : "denominator", ' +
-      '"amcare_xml_path" :[ ScoreCard, ReportingCategories, ' + title + ' ]' +
+      '"doctor" : "'   + pid       + '", ' +
+      '"title" : "'    + title     + '", ' +
+      '"date" : "'     + date      + '", ' +
+      '"category" : "' + category  + '", ' +
+      '"result" : "denominator" ' +
     '}',
     denominator
   );
   emit(
     '{ '+
-    '"doctor" : "' + pid  + '", ' +
-    '"date" : "'   + date + '", ' +
-    '"type" : "'   + type + '", ' +
-    '"result" : "numerator", ' +
-      '"amcare_xml_path" : [ScoreCard, ReportingCategories, ' + title +' ] ' +
+      '"doctor" : "'   + pid      + '", ' +
+      '"title" : "'    + title    + '", ' +
+      '"date" : "'     + date     + '", ' +
+      '"category" : "' + category + '", ' +
+      '"result" : "numerator" '  +
     '}',
     numerator
   );
