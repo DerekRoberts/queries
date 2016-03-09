51a0115988bc211c9589ffa8d76ef21c8abc1658
Derek Roberts <derek.roberts@gmail.com>
2016-03-08 21:37:43 -0800

    PatientCounts output revised, closer to AMCARE data

--- PatientCounts.js
+++ PatientCounts.js
@@ -1,14 +1,15 @@
 /**
- * Query Title: Retro-PDC-001
+ * Query Title: HDC-1921
  * Query Type:  Pyramid/count
  * Description: Population Pyramid
  */
 function map( patient )
 {
   // Physician ID, value type and time interval
-  var pid      = patient.json.primary_care_provider_id;
-  var type     = 'count';
-  var interval = 'TenYearRanges';
+  var pid        = patient.json.primary_care_provider_id;
+  var title      = 'PatientCounts';
+  var category   = 'TenYearRanges';
+  var open_ended = 'false';
 
   // Indexes
   var index_gdrs;
@@ -24,19 +25,19 @@ function map( patient )
     index_gdrs = 2;
 
   // Constants
-  var all_gdrs =[ 'Female', 'Male', 'Unspecified' ];
-  var all_ages =[ 'ZeroToNine', 'TenToNineteen', 'TwentyToTwentyNine', 'ThirtyToThirtyNine', 'FortyToFortyNine',
-                  'FiftyToFiftyNine', 'SixtyToSixtyNine', 'SeventyToSeventyNine', 'SeventyToSeventyNine', 'NinetyToOneHundredAndOlder' ];
+  var genders =[ 'Female', 'Male', 'Unspecified' ];
+  var age_min =[ '0', '10', '20', '30', '40', '50', '60', '70', '80', '90' ];
+  var age_max =[ '9', '19', '29', '39', '49', '59', '69', '79', '89', '120' ];
 
   // Start, end (now) and counter dates
-  var start = new Date(2016, 2, 1);//Remember months are zero indexed
-  var end = new Date().getTime();
-  var i = new Date( start.getTime() );
+  var start = new Date( 2016, 2, 1 );//Remember months are zero indexed
+  var end   = new Date().getTime();
+  var i     = new Date( start.getTime() );
   //
   // Monthly results
   for( ; i.getTime() < end; i.setMonth( i.getMonth() + 1 ))
   {
-    // Array to emit from (by age range, then FEMALE/MALE/OTHER/UNDEF)
+    // Array to emit from (by age range, then F/M/UN)
     var mask = [
       [ 0, 0, 0 ],
       [ 0, 0, 0 ],
@@ -73,19 +74,20 @@ function map( patient )
     }
 
     // Output mask, arranged by g=gender and a=age
-    for( var a = 0; a < all_ages.length; a++ )
+    for( var a = 0; a < age_min.length; a++ )
     {
-      for( var g = 0; g < all_gdrs.length; g++ )
+      for( var g = 0; g < genders.length; g++ )
       {
         emit(
           '{ '+
-            '"doctor" : "' + pid           + '", ' +
-            '"date" : "'   + i_date        + '", ' +
-            '"type" : "'   + type          + '", ' +
-            '"gender" : "' + all_gdrs[ g ] + '", ' +
-            '"range" : "'  + interval      + '", ' +
-            '"age_r" : "'  + all_ages[ a ] + '", ' +
-            '"amcare_xml_path" : "ScoreCard.PatientCounts.'  + all_gdrs[ g ] + "." + interval + "." + age_r + '" ' +
+            '"doctor" : "'     + pid          + '", ' +
+            '"title" : "'      + title        + '", ' +
+            '"date" : "'       + i_date       + '", ' +
+            '"category" : "'   + category     + '", ' +
+            '"open_ended" : "' + open_ended   + '", ' +
+            '"age_min" : "'    + age_min[ a ] + '", ' +
+            '"age_max" : "'    + age_max[ a ] + '", ' +
+            '"gender" : "'     + genders[ g ] + '"' +
           '}',
           mask[ a ][ g ]
         );
