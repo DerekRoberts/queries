2a358afe17e74fda42318a6dee2fc4aa04ffb462
Derek Roberts <derek.roberts@gmail.com>
2016-03-17 16:45:29 -0700

    +functions/ages.js - retro age fns - isRange, isMax and isMin

---
+++ functions/ages.js
@@ -0,0 +1,46 @@
+/**
+* T/F: Does the patient fall in this age range?
+*   - missing input returns false and emits an error
+*
+* @param pt - the hQuery patient object to consider
+* @param ageMin - lower (inclusive) bound of the age range
+* @param ageMax - upper (inclusive) bound of the age range
+* @param atDate - reference date for function
+*
+* @return (boolean) - true if in age range, false if not or on error
+*/
+var ages = ages ||{};
+ages.isRange = function( patient, atDate, ageMin, ageMax ){
+	// Check input
+	if(
+		patient === undefined || patient === null ||
+		atDate === undefined || atDate === null ||
+		ageMin === undefined || ageMin === null ||
+		ageMax === undefined || ageMax === null
+	){
+		return error( "isAgeRange input" );
+	}
+
+	var ageNow = patient.age( atDate );
+	if(
+		ageNow === undefined || ageNow === null ||
+		typeof ageNow != 'number'
+	){
+		return error( "patient.age" );
+	}
+	else {
+		return (( ageMin <= ageNow )&&( ageNow <= ageMax ));
+	}
+}
+
+// Call isRange, use default for ageMin
+ages.isMax = function( patient, atDate, ageMax ){
+	var ageMin = patientDefaults.ageMin();
+	return this.isRange( patient, atDate, ageMin, ageMax );
+}
+
+// Call isRange, use default for ageMax
+ages.isMin = function( patient, atDate, ageMin ){
+	var ageMax = patientDefaults.ageMax();
+	return this.isRange( patient, atDate, ageMin, ageMax );
+}
