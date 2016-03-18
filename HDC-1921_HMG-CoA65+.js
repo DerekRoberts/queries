702660e9cfb4b38da2e4e4f99a945149f48a5d14
Derek Roberts <derek.roberts@gmail.com>
2016-03-18 15:47:50 -0700

    HDC-1921 - simplified, a bit more efficient

--- HDC-1921_HMG-CoA65+.js
+++ HDC-1921_HMG-CoA65+.js
@@ -9,37 +9,31 @@
 function map( patient ){
 
   // Physician ID, value type and time interval
-  var pid      = patient.json.primary_care_provider_id;
-  var title    = 'HDC-1921';
-  var category = 'ReportingCategories';
-  var type     = 'ratio';
+  var pid    = patient.json.primary_care_provider_id,
+    title    = 'HDC-1921',
+    category = 'ReportingCategories';
 
-  // Denominator, numerator and other for loop variables
-  var i_age       = "Value not provided";
-  var i_date      = "Value not provided";
-  var denominator = "Value not provided";
-  var numerator   = "Value not provided";
+  // Denominator, numerator and any other for loop variables
+  var i_date    = "Value not provided",
+    denominator = "Value not provided",
+    numerator   = "Value not provided";
 
   // Start, end (now) and counter dates for monthly results
-  var start = new Date( 2016, 2, 1 );//Remember months are zero indexed
-  var end   = new Date().getTime();
-  var i     = new Date( start.getTime() );
-  for( ; i.getTime() < end; i.setMonth( i.getMonth() + 1 ))
+  var i = defaults.dates.start();
+    end = defaults.dates.end();
+  for( ; i < end; i.setMonth( i.getMonth() + 1 ))
   {
-    // Store age and date at time i
-    i_age  = patient.age( i );
-    i_date = i.getTime();
-
     // Active patient? Age 65+?  Active statin?
     denominator = activePatient( patient, i ) && ages.isMin( patient, i, 65 );
     numerator   = denominator && medications.hasActiveStatin( patient, i );
 
     // Emits
+    date = i.getTime();
     emit(
       '{ ' +
         '"doctor" : "'   + pid       + '", ' +
         '"title" : "'    + title     + '", ' +
-        '"date" : "'     + i_date    + '", ' +
+        '"date" : "'     + date      + '", ' +
         '"category" : "' + category  + '", ' +
         '"result" : "denominator" ' +
       '}',
@@ -49,7 +43,7 @@ function map( patient ){
       '{ '+
         '"doctor" : "'   + pid      + '", ' +
         '"title" : "'    + title    + '", ' +
-        '"date" : "'     + i_date   + '", ' +
+        '"date" : "'     + date     + '", ' +
         '"category" : "' + category + '", ' +
         '"result" : "numerator" '  +
       '}',
