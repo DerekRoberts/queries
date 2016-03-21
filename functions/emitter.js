bd8a48ee6c0a5c327df107d7a1f16b1587574bea
Derek Roberts <derek.roberts@gmail.com>
2016-03-21 14:54:49 -0700

    functions/emitter.js - standard emitter, for ratio queries so far

---
+++ functions/emitter.js
@@ -0,0 +1,27 @@
+var emitter = emitter ||{};
+emitter.ratio = function( patient, query ){
+	// Emit variables
+	var denominator = 'Value not provided',
+	  numerator     = 'Value not provided',
+	  jsonEmit      = {
+	    doctor      : patient.json.primary_care_provider_id,
+	    category    : 'ReportingCategories',
+	    result      : 'Value not provided'
+	  };
+
+	// Counter (date) variables
+	var i = defaults.dates.start(),
+		end = defaults.dates.end();
+	for( ; i < end; i.setMonth( i.getMonth() + 1 ))
+	{
+		denominator = query.denominator( patient, i );
+		numerator   = query.numerator( patient, i, denominator );
+
+		jsonEmit.date = i.getTime().toString();
+		jsonEmit.result = "denominator";
+		emit( JSON.stringify( jsonEmit ), denominator );
+
+		jsonEmit.result = "numerator";
+		emit( JSON.stringify( jsonEmit ), numerator );
+	}
+};
