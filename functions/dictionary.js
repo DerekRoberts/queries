903bb3ab913bbe575656f588574b49d408fbf607
Derek Roberts <derek.roberts@gmail.com>
2016-03-22 23:14:17 -0700

    functions/dictionary.js - added condition objects (ICD9, SNOMEDCT)

--- functions/dictionary.js
+++ functions/dictionary.js
@@ -14,7 +14,7 @@ dictionary.meds = dictionary.meds ||{
   angeotensin2Antagonist               : "^C09CA.*",
   betaBlocker                          : "^C07[ABCDEF]B.*",
   biguanide                            : "^A10BA.*",
-  dihydropyridineCalciumChannelBLocker : "^C08[CG]A.*",
+  dihydropyridineCalciumChannelBlocker : "^C08[CG]A.*",
   naturalOpiumAlkaloid                 : "^N02AA.*",
   PPI                                  : "^A02BC.*",
   statin                               : "^C10(AA|BX|BA).*",
@@ -22,9 +22,37 @@ dictionary.meds = dictionary.meds ||{
   thyroidHormone                       : "^H03AA.*"
 };
 
-// dictionary.meds.ace.code.loinc, whoATC, etc.
-// dictionary.meds.ace.male.min
-
+// http://www.cms.gov/medicare-coverage-database/staticpages/icd-9-code-lookup.aspx
+dictionary.conditions = dictionary.conditions ||{
+  asthma : {
+    ICD9     : "^493.*"
+  },
+  chronicKidneyDisease : {
+    ICD9     : "^58[1-3].*|^585.*|^58[7-8].*"
+  },
+  COPD : {
+    ICD9     : "^49[1246]$"
+  },
+  congestiveHeartFailure : {
+    ICD9     : "^428.*|^428$"
+  },
+  depression : {
+    ICD9     : "^311.*"
+  },
+  diabetes : {
+    ICD9     : "^250.*"
+  },
+  hepatitisC : {
+    ICD9     : "^070.41|^070.44|^070.51|^070.54|^070.70|^070.71*"
+  },
+  hypertension : {
+    ICD9     : "^493.*",
+    SNOMEDCT : "38341003"
+  },
+  rheumatoid : {
+    ICD9     : "^714.*"
+  }
+};
 
 dictionary.defaults = dictionary.defaults ||{
   // Min and max ages supported by hQuery
