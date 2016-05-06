98ddfcf3d37770898102e266922e67510aefc1f6
Derek Roberts <derek.roberts@gmail.com>
2016-05-06 11:58:30 -0700

    +HDC-0020 - query, pneumocolccal vaccine, 65+

---
+++ queries/HDC-0020_PH-recentColonScreening.js
@@ -0,0 +1,28 @@
+/**
+ * Query Title: HDC-0014
+ * Query Type:  Ratio
+ * Initiative:  Population Health
+ * Description: Of active patients, 65+,
+ *              how many have a pneumococcal vaccine?
+ */
+function map( patient ){
+
+  // Query logic
+  var query = {
+
+    // Age restraint and min med count
+    minAge : 65,
+    imm    : dictionary.immunizations.pneumococcal,
+
+    // Active patient? Age?
+    denominator: function( patient, date, err ){
+      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.minAge );
+      },
+    // Active statin?
+    numerator: function( patient, date, denominator, err ) {
+      return denominator && immunizations.hasActiveImmunization( patient, date, this.imm, err );
+    }
+  };
+  // Emit results based on query above
+  emitter.ratio( patient, query );
+}
