b43a7c92d02d1fe81aae0347ab9106b27c14008d
Derek Roberts <derek.roberts@gmail.com>
2016-05-06 11:57:31 -0700

    HDC-0014 - added active patient restraint (wasn't in first request)

--- queries/HDC-0014_PH-pneumococcalMap.js
+++ queries/HDC-0014_PH-pneumococcalMap.js
@@ -2,7 +2,7 @@
  * Query Title: HDC-0014
  * Query Type:  Ratio
  * Initiative:  Population Health
- * Description: Of patients 65+,
+ * Description: Of active patients, 65+,
  *              how many have a pneumococcal vaccine?
  */
 function map( patient ){
@@ -11,12 +11,12 @@ function map( patient ){
   var query = {
 
     // Age restraint and min med count
-    minAge       : 65,
-    imm : dictionary.immunizations.pneumococcal,
+    minAge : 65,
+    imm    : dictionary.immunizations.pneumococcal,
 
     // Active patient? Age?
     denominator: function( patient, date, err ){
-      return profile.ages.isMin( patient, date, this.minAge );
+      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.minAge );
       },
     // Active statin?
     numerator: function( patient, date, denominator, err ) {
