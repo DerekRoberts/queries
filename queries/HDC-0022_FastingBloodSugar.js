419840a708ff08e1db01929a6007ce49848c97ae
Michael Townrow <wooferm@users.noreply.github.com>
2016-05-11 10:50:43 -0700

    Added Query HDC-0022 Fasting Blood Sugar

---
+++ queries/HDC-0022_FastingBloodSugar.js
@@ -0,0 +1,44 @@
+/**
+ * Query Title: HDC-0022 Query Type: Ratio Description: Fasting BS 3y, 46+
+ */
+function map(patient) {
+
+    // Query logic
+    var query = {
+
+	// Age restraint
+	minAge : 46,
+	fastingBloodSugar : dictionary.labs.glucoseFasting,
+
+	/**
+	 * Denominator
+	 * 
+	 * Base criteria: - 46+ years old
+	 */
+	denominator : function(patient, date, errorContainer) {
+	    return profile.active(patient, date)
+		    && profile.ages.isMin(patient, date, this.minAge);
+	},
+
+	/**
+	 * Numerator
+	 * 
+	 * Additional criteria: - has had fasting glucose (blood sugar) recorded
+	 * --> tested in the last three years
+	 */
+	numerator : function(patient, date, denominator, errorContainer) {
+	    var minDate = utils.yearsBefore(date, 3);
+	    var maxDate = date;
+
+	    var fastingBloodSugar = labs.hasLabInDateRange(
+		    patient, minDate, maxDate, this.fastingBloodSugar,
+		    errorContainer);
+
+	    return (denominator && fastingBloodSugar);
+	},
+
+    };
+
+    // Emit results based on query above
+    emitter.ratio(patient, query);
+}
