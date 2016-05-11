f3b6061407bc533adbe58315ba0835293bb1cd79
Michael Townrow <wooferm@users.noreply.github.com>
2016-05-11 11:19:25 -0700

    Added query HDC-0025 Diabetes and Hemoglobin A1C

---
+++ queries/HDC-0025_DiabetesHgbA1C.js
@@ -0,0 +1,43 @@
+/**
+ * Query Title: HDC-0025 Query Type: Ratio Description: A1C Check 6m
+ */
+function map(patient) {
+
+    // Query logic
+    var query = {
+
+	diabetes : dictionary.conditions.diabetes,
+	hemoglobinA1C : dictionary.labs.hemoglobinA1C,
+
+	/**
+	 * Denominator
+	 * 
+	 * Base criteria: - diagnosed with diabetes
+	 */
+	denominator : function(patient, date, errorContainer) {
+	    return profile.active(patient, date)
+		    && conditions.hasActiveCondition(patient, date,
+			    this.diabetes, errorContainer);
+
+	},
+
+	/**
+	 * Numerator
+	 * 
+	 * Additional criteria: - HGBA1C recorded ---> in last six months
+	 */
+	numerator : function(patient, date, denominator, errorContainer) {
+	    var minDate = utils.monthsBefore(date, 6);
+	    var maxDate = date;
+
+	    var hemoglobinA1C = labs.hasLabInDateRange(patient, minDate,
+		    maxDate, this.hemoglobinA1C, errorContainer);
+
+	    return (denominator && hemoglobinA1C);
+	},
+
+    };
+
+    // Emit results based on query above
+    emitter.ratio(patient, query);
+}
