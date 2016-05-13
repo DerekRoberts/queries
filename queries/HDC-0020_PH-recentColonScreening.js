1b0510c6a3b7292984db617a56f00ce4e1b17480
Derek Roberts <derek.roberts@gmail.com>
2016-05-13 15:19:39 -0700

    HDC-0020 - corrected, was a duplicate of another query

--- queries/HDC-0020_PH-recentColonScreening.js
+++ queries/HDC-0020_PH-recentColonScreening.js
@@ -2,27 +2,37 @@
  * Query Title: HDC-0014
  * Query Type:  Ratio
  * Initiative:  Population Health
- * Description: Of active patients, 65+,
- *              how many have a pneumococcal vaccine?
+ * Description: Of active patients, 50 <= age < 75,
+ *              how many have a colon cancer screening in the last two years
+ *              or a colonoscopy or sigmoidoscopy in the last five years?
  */
-function map( patient ){
+function map( pt ){
 
   // Query logic
   var query = {
 
-    // Age restraint and min med count
-    minAge : 65,
-    imm    : dictionary.immunizations.pneumococcal,
+    // Base variables
+    minAge : 0,
+    maxAge : 174.999,
+    labOne : dictionary.labs.colonCancer,
+    labTwo : dictionary.labs.sigmoidoscopyOrColonoscopy,
 
-    // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.minAge );
+    // Denominator logic
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) && profile.ages.isRange( pt, date, this.minAge, this.maxAge, err );
       },
-    // Active statin?
-    numerator: function( patient, date, denominator, err ) {
-      return denominator && immunizations.hasActiveImmunization( patient, date, this.imm, err );
+    // Numerator logic
+    numerator: function( pt, date, denominator, err ) {
+      var back2y = utils.yearsBefore( date, 2 );
+      var back5y = utils.yearsBefore( date, 5 );
+      var cutoff = date;
+
+      var checkOne = labs.hasLabInDateRange( pt, back2y, cutoff, this.labOne, err );
+      var checkTwo = labs.hasLabInDateRange( pt, back5y, cutoff, this.labTwo, err );
+
+      return denominator &&( checkOne || checkTwo );
     }
   };
   // Emit results based on query above
-  emitter.ratio( patient, query );
+  emitter.ratio( pt, query );
 }
