d9f31d91f26ff790cc4c063c28575bfc9b758c01
Derek Roberts <derek.roberts@gmail.com>
2016-05-20 16:21:35 -0700

    +HDC-1902 ! colonoscopy or sigmoidoscopy 50<=age<75 pneumococcal vaccine

---
+++ queries/HDC-1902_PH-colonScreening.js
@@ -0,0 +1,33 @@
+/**
+ * Query Title: HDC-0014
+ * Query Type:  Ratio
+ * Initiative:  Population Health
+ * Description: Of active patients, 50 <= age < 75,
+ *              how many have a colon cancer screening in the last two years
+ *              or a colonoscopy or sigmoidoscopy in the last five years?
+ */
+function map( pt ){
+
+  // Query logic
+  var query = {
+
+    // Base variables
+    minAge : 0,
+    maxAge : 174.999,
+    lab    : dictionary.labs.sigmoidoscopyOrColonoscopy,
+
+    // Denominator logic
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) && profile.ages.isRange( pt, date, this.minAge, this.maxAge, err );
+      },
+    // Numerator logic
+    numerator: function( pt, date, denominator, err ) {
+      var minDate = utils.yearsBefore( date, 10 );
+      var maxDate = date;
+
+      return denominator && !labs.hasLabInDateRange( pt, minDate, maxDate, this.lab, err );
+    }
+  };
+  // Emit results based on query above
+  emitter.ratio( pt, query );
+}
