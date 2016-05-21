125e1d6898e9f85471de267af0edc97317905912
Derek Roberts <derek.roberts@gmail.com>
2016-05-20 22:21:23 -0700

    generateDirectives.sh can now tell ratio from count queries

--- util/generateDirectives.sh
+++ util/generateDirectives.sh
@@ -5,7 +5,7 @@ set -euo pipefail nounset
 DIRECTIVES="../directives"
 QUERY_LIST=$(find ../queries/ -regextype posix-extended -regex ".*./HDC-[0-9]{4}.*\.js")
 
-cd ${DIRECTIVES}
+#cd ${DIRECTIVES}
 for q in ${QUERY_LIST}
 do
 	FILE=${q#../*/*}
@@ -13,22 +13,40 @@ do
 	NAME=${FILE%%_*}
 	DESC=${FULL#*_}
 	SAVE=${NAME}.json
-	if [ -f ./${SAVE} ]
+	HERE=${DIRECTIVES}/${SAVE}
+	if [ -f ./${HERE} ]
 	then
 		echo "${SAVE} already exists"
 	else
-		(
-			echo -e '{'
-			echo -e '\t"type"         : "QUERY",'
-			echo -e '\t"name"         : "'${NAME}_${DESC}'",'
-			echo -e '\t"title"        : "'${NAME}'",'
-			echo -e '\t"description"  : "'${DESC}'",'
-			echo -e '\t"display_name" : "'${DESC}'",'
-			echo -e '\t"query_type"   : "RATIO",'
-			echo -e '\t"map"          : "queries/'${FULL}'.js",'
-			echo -e '\t"reduce"       : "queries/ReduceRatio.js"'
-			echo -e '}'
-		) > ./${SAVE}
+		if( grep 'emitter.ratio' ${q})
+		then
+			(
+				echo -e '{'
+				echo -e '\t"type"         : "QUERY",'
+				echo -e '\t"name"         : "'${NAME}_${DESC}'",'
+				echo -e '\t"title"        : "'${NAME}'",'
+				echo -e '\t"description"  : "'${DESC}'",'
+				echo -e '\t"display_name" : "'${DESC}'",'
+				echo -e '\t"query_type"   : "RATIO",'
+				echo -e '\t"map"          : "queries/'${FULL}'.js",'
+				echo -e '\t"reduce"       : "queries/ReduceRatio.js"'
+				echo -e '}'
+			) > ${HERE}
+		else
+			(
+				echo -e '{'
+				echo -e '\t"type"         : "QUERY",'
+				echo -e '\t"name"         : "'${NAME}_${DESC}'",'
+				echo -e '\t"title"        : "'${NAME}'",'
+				echo -e '\t"description"  : "'${DESC}'",'
+				echo -e '\t"display_name" : "'${DESC}'",'
+				echo -e '\t"query_type"   : "STRATIFIED",'
+				echo -e '\t"map"          : "queries/'${FULL}'.js",'
+				echo -e '\t"reduce"       : "queries/ReduceCount.js"'
+				echo -e '}'
+			) > ${HERE}
+
+		fi
 		echo "${SAVE} successfully created"
 	fi
 done
