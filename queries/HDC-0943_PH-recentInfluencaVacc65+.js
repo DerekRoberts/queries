274cd23b12a7a0429aae64244943f2720f2a5318
Derek Roberts <derek.roberts@gmail.com>
2016-05-24 13:36:46 -0700

    +HDC-0943 - ifluenza vaccine

---
+++ queries/HDC-0943_PH-recentInfluencaVacc65+.js
@@ -0,0 +1,31 @@
+/**
+* Query Title: HDC-0943
+* Query Type:  Ratio
+* Initiative:  Population Health
+* Description: Of active patients, 65+,
+*              how many have an influenza vaccination in the last 1y?
+*/
+function map( patient ){
+
+  // Query logic
+  var query = {
+
+    // Variables
+    ageMin  : 65,
+    codeSet : dictionary.immunizations.influenza,
+
+    // Active patient? Thing?
+    denominator: function( patient, date ){
+      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.ageMin );
+    },
+    // Other things?
+    numerator: function( patient, date, denominator, errorContainer ) {
+      minDate = utils.yearsBefore( date, 1 );
+      maxDate = date;
+
+      return denominator && immunizations.hasActiveImmunizationInDateRange( patient, minDate, maxDate, this.codeSet, errorContainer );
+    }
+  };
+  // Emit results based on query above
+  emitter.ratio( patient, query );
+}
