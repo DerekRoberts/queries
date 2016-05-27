8336b121f7211e4bd699ee4e78caf6e94fc8f467
Derek Roberts <derek.roberts@gmail.com>
2016-05-27 13:38:26 -0700

    medications.js +.hasActiveMedRangeMinDaily(), max and min for total daily dosages

--- functions/medications.js
+++ functions/medications.js
@@ -8,7 +8,7 @@ var medications = medications || {};
 /**
  * Returns whether the patient passed has the medication defined by medInfo on
  * the date passed
- * 
+ *
  * @param patient
  *                hQuery patient object
  * @param date
@@ -29,7 +29,7 @@ medications.hasActiveMed = function(patient, date, medInfo, errorContainer) {
 /**
  * Returns whether the patient passed has the medication defined by medInfo on
  * the date passed with a dose in the range passed
- * 
+ *
  * @param patient
  *                hQuery patient object
  * @param date
@@ -94,7 +94,7 @@ medications.hasActiveMedRange = function(patient, date, medInfo, doseMin,
 /**
  * Returns whether the medication entry passed is a match for any of the codes
  * defined in medInfo
- * 
+ *
  * @param med
  *                single medication entry from hQuery patient object
  * @param medInfo
@@ -102,7 +102,7 @@ medications.hasActiveMedRange = function(patient, date, medInfo, doseMin,
  *                medication for which to search
  * @param errorContainer
  *                ErrorContainer to use for storing any errors or output
- * 
+ *
  */
 medications.isCodeMatch = function(med, medInfo, errorContainer) {
     // Check if it matches one of the codes defined in medInfo
@@ -129,12 +129,12 @@ medications.isCodeMatch = function(med, medInfo, errorContainer) {
     // We did not find a match in any of the codesets defined for the
     // Med
     return false;
-}
+};
 
 /**
  * Examines the medication entry passed and returns whether it contains a valid
  * dose in the range dosemin =< med dose <= doseMax
- * 
+ *
  * @param med
  *                single medication entry from hQuery patient object
  * @param doseMin
@@ -143,7 +143,7 @@ medications.isCodeMatch = function(med, medInfo, errorContainer) {
  *                Maximum valid dose
  * @param errorContainer
  *                ErrorContainer to use for storing any errors or output
- * 
+ *
  */
 medications.isDoseInRange = function(med, doseMin, doseMax, errorContainer) {
     // check if there is a dose defined
@@ -160,18 +160,18 @@ medications.isDoseInRange = function(med, doseMin, doseMax, errorContainer) {
 	// No dose was defined
 	return false;
     }
-}
+};
 
 /**
  * Returns whether the medication entry passed is active on the date passed
- * 
+ *
  * @param med
  *                single medication entry from hQuery patient object
  * @param date
  *                Effective date for which data should be examined
  * @param errorContainer
  *                ErrorContainer to use for storing any errors or output
- * 
+ *
  */
 medications.isActiveMed = function(med, date, errorContainer) {
     // check for valid input, if invalid then we can't operate on the
@@ -227,7 +227,7 @@ medications.isActiveMed = function(med, date, errorContainer) {
 
 /**
  * Returns whether the patient has at least minCount active medications.
- * 
+ *
  * @param patient
  *                hQuery patient object
  * @param date
@@ -260,4 +260,170 @@ medications.activeMedMin = function(patient, date, minCount, errorContainer) {
 
     // Return whether there are enough meds to meet the count
     return activeMeds.length >= minCount;
+};
+
+
+/**
+ * Returns whether a patient has a daily dose of a med in a specified range
+ *
+ * @param patient
+ *                hQuery patient object
+ * @param date
+ *                Effective date for which data should be examined
+ * @param medInfo
+ *                Medication information object to look for
+ * @param dailyMin
+ *                Minimum valid daily dose
+ * @param dailyMax
+ *                Maximum valid daily dose
+ * @param expectedMeasurement
+ *                Dose measurement (e.g. MG, µG, mmol/L, g/L)
+ * @param errorContainer
+ *                ErrorContainer to use for storing any errors or output
+ */
+medications.hasActiveMedRangeDaily = function( patient, date, medInfo, dailyMin,
+	dailyMax, expectedMeasurement, errorContainer
+){
+    // Check input
+    if (utils.isUndefinedOrNullAndLog(
+	    "Invalid data passed to medications hasActiveMedRangeDaily",
+	    utils.invalid, errorContainer, [ patient, "patient" ], [ date,
+		    "date" ], [ medInfo, "medInfo" ], [ dailyMin, "dailyMin" ], [
+		    dailyMax, "dailyMax" ])) {
+	return false;
+    }
+
+    // Get patient medication list
+    var meds = patient.medications();
+
+    if (utils.isUndefinedOrNull(meds) || (meds.length === 0)) {
+	return utils.invalid("Patient has no meds", errorContainer);
+    }
+
+    // Filter meds list to those that match one of the codes defined for the
+    // med. Implemented as a filter so that all meds will be checked and any
+    // data issues found
+    var matchingActiveMeds = meds.filter(function(med) {
+	if (medications.isCodeMatch(med, medInfo, errorContainer)
+		&& medications.isActiveMed(med, date, errorContainer)) {
+	    // Med is a code match, active for the date being examined, and has
+	    // a dose in the required range.
+	    return true;
+	} else {
+	    // Med either is not a code match, is not active for the date being
+	    // examined, or does not have a valid dose in the required range and
+	    // is therefore not a match
+	    return false;
+	}
+    });
+
+    // Daily dose total and loop variables
+    var dailyDose = 0;
+    var atFreq, freqUnit, freqValue, dosePills, doseAmount, doseUnit;
+
+    // Iterate through meds
+    matchingActiveMeds.forEach( function( med ){
+            // Check values
+            if( !utils.isUndefinedOrNullPath(
+                    [ med, ".json.values" ],
+                    [ med, ".json.administrationTiming.frequency.numerator.value" ],
+                    [ med, ".json.administrationTiming.frequency.denominator.unit"],
+                    [ med, ".json.administrationTiming.frequency.denominator.value"],
+                    [ med.json.values[0], ".scalar" ],
+                    [ med.json.values[0], ".units" ]
+            )){
+                    // Store values
+                    atFreq     = med.json.administrationTiming.frequency;
+                    freqUnit   = atFreq.denominator.unit.toString();
+                    freqValue  = atFreq.denominator.value;
+                    dosePills  = atFreq.numerator.value;
+                    doseAmount = med.json.values[0].scalar;
+                    doseUnit   = med.json.values[0].units.toString().toLowerCase();
+
+                    // Check values
+                    if(
+                            // Non-zero
+                            ( dosePills * doseAmount * freqValue === 0)||
+
+                            // Verify expected measurement unit
+                            ( doseUnit !== expectedMeasurement.toLowerCase() )||
+
+                            // Expecting d or h
+                            !freqUnit.match( /^[dh]$/i )
+                    ){
+                            // Emit error
+                            emit( "Medication object has unexpected values", -1 );
+                            return;
+                    }
+
+                    // Tally up dose depending on daily (d) or hourly (h) rate
+                    if( freqUnit === 'd' ){
+                            dailyDose += dosePills * doseAmount * freqValue;
+                    }
+                    else if( freqUnit === 'h' ){
+                            dailyDose += dosePills * doseAmount *( 24 / freqValue );
+                    }
+            }
+    });
+
+    // Return whether dailyDost is in the requested range
+    if(( dailyMin <= dailyDose )&&( dailyDose <= dailyMax )){
+	return true;
+    } else {
+	return false;
+    }
+};
+
+
+/**
+ * Returns whether a patient has a daily dose of a med above a min value
+ *
+ * @param patient
+ *                hQuery patient object
+ * @param date
+ *                Effective date for which data should be examined
+ * @param medInfo
+ *                Medication information object to look for
+ * @param dailyMin
+ *                Minimum valid daily dose
+ * @param expectedMeasurement
+ *                Dose measurement (e.g. MG, µG, mmol/L, g/L)
+ * @param errorContainer
+ *                ErrorContainer to use for storing any errors or output
+ */
+medications.hasActiveMedMinDaily = function(
+        patient, date, medInfo, dailyMin, expectedMeasurement, errorContainer
+){
+        var dailyMax = defaults.doses.max;
+        return medications.hasActiveMedRangeDaily(
+                patient, date, medInfo, dailyMin, dailyMax,
+                expectedMeasurement, errorContainer
+        );
+};
+
+
+/**
+ * Returns whether a patient has a daily dose of a med below a max value
+ *
+ * @param patient
+ *                hQuery patient object
+ * @param date
+ *                Effective date for which data should be examined
+ * @param medInfo
+ *                Medication information object to look for
+ * @param dailyMax
+ *                Maximum valid daily dose
+ * @param expectedMeasurement
+ *                Dose measurement (e.g. MG, µG, mmol/L, g/L)
+ * @param errorContainer
+ *                ErrorContainer to use for storing any errors or output
+ */
+medications.hasActiveMedMaxDaily = function(
+        patient, date, medInfo, dailyMax, expectedMeasurement, errorContainer
+){
+        var dailyMin = defaults.doses.min;
+        return medications.hasActiveMedRangeDaily(
+                patient, date, medInfo, dailyMin, dailyMax,
+                expectedMeasurement, errorContainer
+        );
 };
