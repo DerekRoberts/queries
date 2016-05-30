a0f0af1906b4941f51118873e42d1279bd4b85f6
Derek Roberts <derek.roberts@gmail.com>
2016-05-30 13:40:47 -0700

    Corrections to 1154 and 1155

--- queries/HDC-1154_PH-pneumovaxAdministered.js
+++ queries/HDC-1154_PH-pneumovaxAdministered.js
@@ -1,30 +1,32 @@
 /**
- * Query Title: HDC-1155
+ * Query Title: HDC-1154
  * Query Type:  Ratio
  * Initiative:  Population Health
- * Description: Of active patients, 25+,
- *              how many have a tetanus vaccine in the last 10y?
+ * Description: Of active patients, 65+,
+ *              how many have a pneumococcal vaccine in the last 10y?
  */
-function map( patient ){
+function map( pt ){
 
   // Query logic
   var query = {
 
     // Age restraint and min med count
-    minAge : 25,
+    minAge : 65,
     imm    : dictionary.immunizations.pneumococcal,
 
     // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.minAge );
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) &&
+        profile.ages.isMin( pt, date, this.minAge );
       },
-    // Active vaccination?
-    numerator: function( patient, date, denominator, err ) {
+    // Active vaccination in time range?
+    numerator: function( pt, date, denominator, err ) {
       var minDate = utils.yearsBefore( date, 10 );
       var maxDate = date;
-      return denominator && immunizations.hasImmunizationInDateRange( patient, minDate, maxDate, this.imm, err );
+      return denominator &&
+        immunizations.hasImmunizationInDateRange( pt, minDate, maxDate, this.imm, err );
     }
   };
   // Emit results based on query above
-  emitter.ratio( patient, query );
+  emitter.ratio( pt, query );
 }
--- queries/HDC-1155_PH-tetanusVaccinations.js
+++ queries/HDC-1155_PH-tetanusVaccinations.js
@@ -5,26 +5,28 @@
  * Description: Of active patients, 25+,
  *              how many have a tetanus vaccine in the last 10y?
  */
-function map( patient ){
+function map( pt ){
 
   // Query logic
   var query = {
 
     // Age restraint and min med count
     minAge : 25,
-    imm    : dictionary.immunizations.pneumococcal,
+    imm    : dictionary.immunizations.tetatnus,
 
     // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.minAge );
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) &&
+        profile.ages.isMin( pt, date, this.minAge );
       },
-    // Active vaccination?
-    numerator: function( patient, date, denominator, err ) {
+    // Active vaccination in time range?
+    numerator: function( pt, date, denominator, err ) {
       var minDate = utils.yearsBefore( date, 10 );
       var maxDate = date;
-      return denominator && immunizations.hasImmunizationInDateRange( patient, minDate, maxDate, this.imm, err );
+      return denominator &&
+        immunizations.hasImmunizationInDateRange( pt, minDate, maxDate, this.imm, err );
     }
   };
   // Emit results based on query above
-  emitter.ratio( patient, query );
+  emitter.ratio( pt, query );
 }
