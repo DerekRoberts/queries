d727250e54dae4c262271e838c46b89a07e5fa17
Derek Roberts <derek.roberts@gmail.com>
2016-06-02 18:58:09 -0700

    56, 57, 58 - revised formatting for errors

--- queries/HDC-0056_PP-renalMap65.js
+++ queries/HDC-0056_PP-renalMap65.js
@@ -5,30 +5,28 @@
  * Description: Of active patients, 65+, with impaired renal function,
  *              how many are on digoxin?
  */
-function map( patient ){
+function map( pt ){
 
   // Query logic
   var query = {
 
     // Med codes and age restraints
-    medication : dictionary.meds.digoxin,
-    condition  : dictionary.conditions.impairedRenalFunction,
     minAge     : 65,
+    condition  : dictionary.conditions.impairedRenalFunction,
+    medication : dictionary.meds.digoxin,
 
-    // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return
-        profile.active( patient, date ) &&
-        profile.ages.isMin( patient, date, this.minAge ) &&
-        conditions.hasActiveCondition( patient, date, this.condition, err );
+    // Active pt? Age?
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) &&
+        profile.ages.isMin( pt, date, this.minAge ) &&
+        conditions.hasActiveCondition( pt, date, this.condition, err );
     },
     // Active statin?
-    numerator: function( patient, date, denominator, err ) {
-      return
-        denominator &&
-        medications.hasActiveMed( patient, date, this.medication, err );
+    numerator: function( pt, date, denominator, err ) {
+      return denominator &&
+        medications.hasActiveMed( pt, date, this.medication, err );
     }
   };
   // Emit results based on query above
-  emitter.ratio( patient, query );
+  emitter.ratio( pt, query );
 }
--- queries/HDC-0058_PP-secondaryStatinMap65.js
+++ queries/HDC-0058_PP-secondaryStatinMap65.js
@@ -5,30 +5,28 @@
  * Description: Of active patients, 65+, on a statin,
  *              how many have had a cardiac event?
  */
-function map( patient ){
+function map( pt ){
 
   // Query logic
   var query = {
 
     // Med codes and age restraints
+    minAge     : 65,
     medication : dictionary.meds.statin,
     condition  : dictionary.conditions.cardiacEvent,
-    minAge     : 65,
 
-    // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return
-        profile.active( patient, date ) &&
-        profile.ages.isMin( patient, date, this.minAge ) &&
-        medications.hasActiveMed( patient, date, this.medication, err );
+    // Active pt? Age?
+    denominator: function( pt, date, err ){
+      return profile.active( pt, date ) &&
+        profile.ages.isMin( pt, date, this.minAge ) &&
+        medications.hasActiveMed( pt, date, this.medication, err );
       },
     // Active statin?
-    numerator: function( patient, date, denominator, err ) {
-      return
-        denominator &&
-        conditions.hasActiveCondition( patient, date, this.condition, err );
+    numerator: function( pt, date, denominator, err ) {
+      return denominator &&
+        conditions.hasActiveCondition( pt, date, this.condition, err );
     }
   };
   // Emit results based on query above
-  emitter.ratio( patient, query );
+  emitter.ratio( pt, query );
 }
