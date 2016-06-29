55776ca6ac95c5f76e1bd4e9a745e6fe76387b7a
Michael Townrow <wooferm@users.noreply.github.com>
2016-06-29 14:34:34 -0700

    Added query HDC-1918 Immunization Classification

---
+++ queries/HDC-1918_ImmunizationClassification.js
@@ -0,0 +1,32 @@
+/**
+ * title : HDC-1918 Immunization Classification
+ * description : Classification of immunizations delivered
+ */
+
+function map(patient) {
+
+    
+    
+    
+    // Query logic
+    var query = {
+
+	codeMap : function(patient, date, errorContainer) {
+	    return immunizations.buildCodeMap(patient, null, date, errorContainer);
+	},
+	
+	denominator : function(patient, date, errorContainer) {
+	    return (profile.active( patient, date ) ? 1 : 0);
+	},
+	
+	numerator : function(patient, date, denominator, codeSet, code, codeCount, errorContainer) {
+	    if(denominator > 0) {
+		return codeCount;
+	    } else {
+		return 0;
+	    }
+	}
+    };
+    // Emit results based on query above
+    emitter.ratioCodeCount(patient, query);
+}
