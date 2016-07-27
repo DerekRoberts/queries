486b194e7d5a662b9562fbc65f2020fada8769eb
Michael Townrow <wooferm@users.noreply.github.com>
2016-07-27 11:02:09 -0700

    Rewrote query HDC-1941 Medication Frequency Data Quality. CHanged the structure and format of the output to conform to a code count type query.

--- queries/HDC-1941_medicationFrequencyDataQuality.js
+++ queries/HDC-1941_medicationFrequencyDataQuality.js
@@ -1,66 +1,83 @@
 /**
  * Query Title: HDC-1941 Medication Frequency Data Quality
- * Query Type: Unique Description: Emits the units of
- * frequency used to describe medications.
- * 
- * This query is completely unique in both the structure and format of its output. 
- * It is also very simple. Therefore it does not use any of the supporting 
- * functions used by nearly all other queries as none of it would be useful and
- * there is no reason to expand the support functionality for this indicator as
- * such functionality would be used only by this indicator.
- *  
- * If this indicator is ever redesigned to output as a ratio, then it should 
- * incorporate use of the common support functionality.
- * 
- *  Note that, due to its unique structure and format, the output
- *  of this query cannot be consumed by any existing tool and is only useful when
- *  run interactively by a user directly within hQuery. 
+ * Query Type: Ratio Code Count - Denominator Not Used 
+ * Emits the units of frequency used to describe medications and their usage.
+ * Note: QUery is not retroactive or date dependent.
  */
 function map(patient) {
+    // Query logic
+    var query = {
 
-    var meds = patient.json.medications;
+	codeMap : function(patient, date, errorContainer) {
+	    // Build map of codeset to be returned
+	    var codesetMap = {};
 
-    if (!meds) {
-	return;
-    }
+	    // Get patient medication list
+	    var meds =  patient.json.medications;
 
-    var med = null;
-    var timing = null;
+	    if (utils.isUndefinedOrNull(meds) || (meds.length === 0)) {
+		utils.invalid("Patient has no meds", errorContainer);
+		return codesetMap;
+	    }
+	    
+	    //Examine each medication entry
+	    var frequencyCode;
+	    var frequencyCodeMap = {};
+	    var timing;
+	    meds.forEach(function(med) {
+		
+		// Determine the frequency code this med should be counted towards  
+		timing = med.administrationTiming;
+		if (!timing.frequency) {
+		    frequencyCode = "noFrequency";
+		} else if (!timing.frequency.numerator) {
+		    frequencyCode = "noFrequencyNumerator";
+		} else if (!timing.frequency.denominator) {
+		    frequencyCode = "noFrequencyDenominator";
+		} else if (!timing.frequency.numerator.value) {
+		    frequencyCode = "noFrequencyNumeratorValue";
+		} else if (!timing.frequency.denominator.unit) {
+		    frequencyCode = "noFrequencyDenominatorUnit";
+		} else if (!timing.frequency.denominator.value) {
+		    frequencyCode = "noFrequencyDenominatorValue";
+		} else {
+		    frequencyCode = "frequencyDenominatorUnit_" + timing.frequency.denominator.unit;
+		}		
+		
+		// Get previous count for frequency code
+		var previousCount;
+		if (frequencyCodeMap[frequencyCode] === undefined) {
+		    previousCount = 0;
+		} else {
+		    previousCount = frequencyCodeMap[frequencyCode] ;
+		}
 
-    for (var m = 0; m < meds.length; m++) {
+		// Add to Map count
+		frequencyCodeMap[frequencyCode] = previousCount + 1;
+		
+		
+	    });
 
-	med = meds[m];
+	    // save back to map
+	    codesetMap["Frequency"] = frequencyCodeMap;
 
-	if (!med.administrationTiming) {
-
-	    emit(patient.json.primary_care_provider_id + "_noTiming", 1);
-	    continue;
-
+	    return codesetMap;
+	},
+	
+	/**
+	 * Always returns 0
+	 */
+	denominator : function(patient, date, errorContainer) {
+	    return 0;
+	},
+	
+	/**
+	 * Always returns the count passed
+	 */
+	numerator : function(patient, date, denominator, codeSet, code, codeCount, errorContainer) {
+		return codeCount;
 	}
-
-	timing = med.administrationTiming;
-
-	if (!timing.frequency) {
-	    emit(patient.json.primary_care_provider_id + "_noFrequency", 1);
-	} else if (!timing.frequency.numerator) {
-	    emit(patient.json.primary_care_provider_id
-		    + "_noFrequencyNumerator", 1);
-	} else if (!timing.frequency.denominator) {
-	    emit(patient.json.primary_care_provider_id
-		    + "_noFrequencyDenominator", 1);
-	} else if (!timing.frequency.numerator.value) {
-	    emit(patient.json.primary_care_provider_id
-		    + "_noFrequencyNumeratorValue", 1);
-	} else if (!timing.frequency.denominator.unit) {
-	    emit(patient.json.primary_care_provider_id
-		    + "_noFrequencyDenominatorUnit", 1);
-	} else if (!timing.frequency.denominator.value) {
-	    emit(patient.json.primary_care_provider_id
-		    + "_noFrequencyDenominatorValue", 1);
-	} else {
-	    emit(patient.json.primary_care_provider_id
-		    + "_frequencyDenominatorUnit_"
-		    + timing.frequency.denominator.unit, 1);
-	}
-    }
+    };
+    // Emit results based on query above
+    emitter.ratioCodeCount(patient, query);
 }
