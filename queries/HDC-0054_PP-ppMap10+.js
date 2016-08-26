f90b7f9837dbb5772a83b7723d032c9eaee75d34
Derek Roberts <derek.roberts@gmail.com>
2016-08-26 12:06:12 -0700

    53, 54 + comments

--- queries/HDC-0054_PP-ppMap10+.js
+++ queries/HDC-0054_PP-ppMap10+.js
@@ -1,28 +1,49 @@
 /**
- * Query Title: HDC-0054
- * Query Type:  Ratio
- * Initiative:  Polypharmacy
- * Description: Of active patients, 65+,
- *              how many have 10+ active meds?
- */
+* Query Title: HDC-0054
+* Query Type:  Ratio
+* Initiative:  Polypharmacy
+* Description: Of active patients, 65+,
+*              how many have 10+ active meds?
+*/
 function map( patient ){
 
-  // Query logic
-  var query = {
+    // Query logic
+    var query = {
 
-    // Age restraint and min med count
-    minAge  : 65,
-    minMeds : 10,
+        /**
+        * Minimum age
+        */
+        minAge  : 65,
 
-    // Active patient? Age?
-    denominator: function( patient, date, err ){
-      return profile.active( patient, date, err ) && profile.ages.isMin( patient, date, this.minAge, err );
-      },
-    // Active statin?
-    numerator: function( patient, date, denominator, err ) {
-      return denominator && medications.activeMedMin( patient, date, this.minMeds, err );
-    }
-  };
-  // Emit results based on query above
-  emitter.ratio( patient, query );
+        /**
+        * Minimim number of medications
+        */
+        minMeds : 10,
+
+        /**
+        * Denominator
+        *
+        * Base criteria:
+        *  - active patient
+        *  - age constraint
+        */
+        denominator: function( patient, date, err ){
+            return profile.active( patient, date, err ) &&
+                profile.ages.isMin( patient, date, this.minAge, err );
+        },
+
+        /**
+        * Numerator
+        *
+        * Additional criteria:
+        * - count of active medications
+        */
+        numerator: function( patient, date, denominator, err ) {
+            return denominator &&
+                medications.activeMedMin( patient, date, this.minMeds, err );
+        }
+    };
+
+    // Emit results based on query above
+    emitter.ratio( patient, query );
 }
