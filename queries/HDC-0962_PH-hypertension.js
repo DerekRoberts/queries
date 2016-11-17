49e4f7446785bbab8bcdd51b1340d5ff535ba04e
Derek Roberts <derek.roberts@gmail.com>
2016-11-16 16:36:38 -0800

    HDC-0962 revised w/o age restriction, for base channel

--- queries/HDC-0962_PH-hypertension.js
+++ queries/HDC-0962_PH-hypertension.js
@@ -2,7 +2,7 @@
 * Query Title: HDC-0962
 * Query Type:  Ratio
 * Initiative:  Population Health
-* Description: Percentage of adults (20+) with hypertension
+* Description: Percentage of adults with hypertension
 */
 function map( patient ){
 
@@ -10,12 +10,11 @@ function map( patient ){
   var query = {
 
     // Variables
-    ageMin  : 20,
     codeSet : dictionary.conditions.hypertension,
 
     // Active patient? Thing?
     denominator: function( patient, date ){
-      return profile.active( patient, date ) && profile.ages.isMin( patient, date, this.ageMin );
+      return profile.active( patient, date );
     },
     // Other things?
     numerator: function( patient, date, denominator, errorContainer ) {
