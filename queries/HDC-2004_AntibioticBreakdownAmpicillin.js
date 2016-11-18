b0352a59a67422b807717c54860ac130679e340e
Derek Roberts <derek.roberts@gmail.com>
2016-11-18 14:57:24 -0800

    +HDC-2001 - HDC-2019 - Antibiotics Breakdown

---
+++ queries/HDC-2004_AntibioticBreakdownAmpicillin.js
@@ -0,0 +1,40 @@
+/**
+ * Query Title: HDC-2004
+ * Query Type:  Ratio
+ * Initiative:  Antibiotic Breakdown
+ * Description: Ampicillin over all antibiotics, back one year
+ */
+function map( patient ){
+
+  // Query logic
+  var query = {
+
+    // Variables
+    medDenom : dictionary.meds.antibiotic,
+    medNum   : dictionary.meds.antibioticAmpicillin,
+    backYrs  : 1,
+
+    // Active patient? Thing?
+    denominator: function( patient, date, errorContainer ){
+      var minDate = utils.yearsBefore( date, this.backYrs );
+      var maxDate = date;
+
+      return medications.hasActiveMedInDateRange( patient, minDate, maxDate,
+        this.medDenom, errorContainer );
+    },
+
+    // Other things?
+    numerator: function( patient, date, denominator, errorContainer ) {
+      var minDate = utils.yearsBefore( date, this.backYrs );
+      var maxDate = date;
+
+      var oneAntibiotic = medications.hasActiveMedInDateRange( patient, minDate,
+        maxDate, this.medNum, errorContainer );
+
+      return oneAntibiotic && denominator;
+    }
+  };
+
+  // Emit results based on query above
+  emitter.ratio( patient, query );
+}
