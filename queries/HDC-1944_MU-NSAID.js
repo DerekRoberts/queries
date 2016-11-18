bc1c7dcef1d7a246630a87e4be02c9d71f0c2468
Derek Roberts <derek.roberts@gmail.com>
2016-11-17 21:07:28 -0800

    +HDC-1944 - Med Use - NSAID (antiinflammatory)

---
+++ queries/HDC-1944_MU-NSAID.js
@@ -0,0 +1,26 @@
+/**
+ * Query Title: HDC-1944
+ * Query Type:  Ratio
+ * Initiative:  Med Use
+ * Description: Percentage of active patients receiving an NSAID
+ */
+function map( patient ){
+
+  // Query logic
+  var query = {
+
+    // Variables
+    codeSet : dictionary.meds.antiinflammatory,
+
+    // Active patient? Thing?
+    denominator: function( patient, date ){
+      return profile.active( patient, date );
+    },
+    // Other things?
+    numerator: function( patient, date, denominator, errorContainer ) {
+      return denominator && medications.hasActiveMed( patient, date, this.codeSet, errorContainer );
+    }
+  };
+  // Emit results based on query above
+  emitter.ratio( patient, query );
+}
