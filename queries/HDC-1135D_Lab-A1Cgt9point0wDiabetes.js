5a11532a212e246f2d0b352edfb5669365726afe
Michael Townrow <wooferm@users.noreply.github.com>
2016-11-24 15:54:10 -0800

    Updated definition of HDC-1135D to examine last lab value at any time instead of any lab in the last 6 months. https://hdcsolution.atlassian.net/browse/PDC-58

--- queries/HDC-1135D_Lab-A1Cgt9point0wDiabetes.js
+++ queries/HDC-1135D_Lab-A1Cgt9point0wDiabetes.js
@@ -26,14 +26,13 @@ function map(patient) {
 	/**
 	 * Numerator
 	 *
-	 * Additional criteria: - HGBA1C recorded ---> in last 6 months with value >= 9.0%
+	 * Additional criteria: - HGBA1C recorded ---> last value at any time had value >= 9.0%
 	 */
 	numerator : function(patient, date, denominator, errorContainer) {
-	    var minDate = utils.monthsBefore(date, 6);
 	    var maxDate = date;
 
-	    var hemoglobinA1C = labs.hasLabInDateRangeWithValue(patient, minDate,
-		    maxDate, this.hemoglobinA1C, this.minValue, null, false, "%", false,  errorContainer);
+	    var hemoglobinA1C = labs.hasLabInDateRangeWithValue(patient, null,
+		    maxDate, this.hemoglobinA1C, this.minValue, null, false, "%", true,  errorContainer);
 
 	    return (denominator && hemoglobinA1C);
 	},
