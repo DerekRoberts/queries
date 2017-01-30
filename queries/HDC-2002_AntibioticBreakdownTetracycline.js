7c6923de97ea42628b3837443b3b204002a2a01a
Michael Townrow <wooferm@users.noreply.github.com>
2017-01-30 15:07:07 -0800

    Updated HDC-2002 to match new definition.
    https://hdcsolution.atlassian.net/browse/PDC-308

--- queries/HDC-2002_AntibioticBreakdownTetracycline.js
+++ queries/HDC-2002_AntibioticBreakdownTetracycline.js
@@ -1,37 +1,40 @@
 /**
- * Query Title: HDC-2002
+ * Query Title: HDC-2002 Tetracycline Use in Past Year - All Ages
  * Query Type:  Ratio
- * Initiative:  Antibiotic Breakdown
- * Description: Tetracycline over all antibiotics, back one year
+ * Domain: Medication Prescribing
+ * Sub Domain: Medication Use - Antibiotics
+ * Description: This measure shows the percentage of active patients with an active medication for Tretracycline in the last year.
  */
 function map( patient ){
 
   // Query logic
   var query = {
 
-    // Variables
-    medDenom : dictionary.meds.antibiotic,
-    medNum   : dictionary.meds.antibioticTetracycline,
-    backYrs  : 1,
+    /**
+	 * Definition of Tetracycline medication from dictionary
+	 */
+	tetracycline   : dictionary.meds.antibioticTetracycline,
 
-    // Active patient? Thing?
-    denominator: function( patient, date, errorContainer ){
-      var minDate = utils.yearsBefore( date, this.backYrs );
-      var maxDate = date;
-
-      return medications.hasActiveMedInDateRange( patient, minDate, maxDate,
-        this.medDenom, errorContainer );
+    /**
+	 * Denominator:
+	 * Count of total number of active patients documented in the EMR.
+	 */
+    denominator : function(patient, date, errorContainer) {
+	    return profile.active(patient, date);
     },
 
-    // Other things?
+    /**
+	 * Numerator:
+	 * Count of the number of active patients that have had a medication for Tretracycline in the last year.
+	 */
     numerator: function( patient, date, denominator, errorContainer ) {
-      var minDate = utils.yearsBefore( date, this.backYrs );
+      var minDate = utils.yearsBefore( date, 1 );
       var maxDate = date;
 
-      var oneAntibiotic = medications.hasActiveMedInDateRange( patient, minDate,
-        maxDate, this.medNum, errorContainer );
+      var onAntibiotic = medications.hasActiveMedInDateRange( patient, minDate,
+        maxDate, this.tetracycline, errorContainer );
 
-      return oneAntibiotic && denominator;
+      return onAntibiotic && denominator;
     }
   };
 
