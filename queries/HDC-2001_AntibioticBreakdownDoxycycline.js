2f2a02106be797b44d7d676cc29c53935f1e4b64
Michael Townrow <wooferm@users.noreply.github.com>
2017-01-30 15:02:34 -0800

    Updated HDC-2001 to match new definition.
    https://hdcsolution.atlassian.net/browse/PDC-307

--- queries/HDC-2001_AntibioticBreakdownDoxycycline.js
+++ queries/HDC-2001_AntibioticBreakdownDoxycycline.js
@@ -38,3 +38,48 @@ function map( patient ){
   // Emit results based on query above
   emitter.ratio( patient, query );
 }
+
+
+/**
+ * Query Title: HDC-2001 Doxycyline Use in Past Year - All Ages
+ * Query Type:  Ratio
+ * Domain: Medication Prescribing
+ * Sub Domain: Medication Use - Antibiotics
+ * Description: This measure shows the percentage of active patients with an active medication for Doxycyline in the last year.
+ */
+function map( patient ){
+
+  // Query logic
+  var query = {
+
+    /**
+	 * Definition of Doxycycline medication from dictionary
+	 */
+	doxycycline   : dictionary.meds.antibioticDoxycycline,
+
+    /**
+	 * Denominator:
+	 * Count of total number of active patients documented in the EMR.
+	 */
+    denominator : function(patient, date, errorContainer) {
+	    return profile.active(patient, date);
+    },
+
+    /**
+	 * Numerator:
+	 * Count of the number of active patients that have had a medication for Doxycyline in the last year.
+	 */
+    numerator: function( patient, date, denominator, errorContainer ) {
+      var minDate = utils.yearsBefore( date, 1 );
+      var maxDate = date;
+
+      var onAntibiotic = medications.hasActiveMedInDateRange( patient, minDate,
+        maxDate, this.doxycycline, errorContainer );
+
+      return onAntibiotic && denominator;
+    }
+  };
+
+  // Emit results based on query above
+  emitter.ratio( patient, query );
+}
